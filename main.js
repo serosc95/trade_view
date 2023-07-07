2516f7d9a86d86d52f8800ed678b749aa46c81ab
serosc95 <oscardleon95@gmail.com>
2023-07-06 20:20:38 -0500

    mejoras de graficas para mejor rendimiento

--- main.js
+++
@@ -1,7 +0,0 @@
-// document.addEventListener("DOMContentLoaded", ready);
-// function ready() {}
-function frameLoaded(){
-    var iframe =  document.getElementById('graph');
-    var frameContent = iframe.contentWindow.document.body.innerHTML;
-    console.log("VALOR", frameContent);
-}
